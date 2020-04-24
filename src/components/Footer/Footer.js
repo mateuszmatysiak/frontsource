4257a960d331f27c2b1febf469742034180c4f57
Mateusz <mateuszmatysiak96@gmail.com>
2020-04-24 13:25:45 +0200

    Refactor Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,68 +1,108 @@
 import React from "react"
 import styled from "styled-components"
-import TwitterIcon from "../../assets/images/twitter.svg"
-import GithubIcon from "../../assets/images/github.svg"
-import LinkedinIcon from "../../assets/images/linkedin.svg"
+import { down } from "styled-breakpoints"
+import { mainNavigation } from "../../utils/navigation"
+import { frontsourceTest } from "../../utils/dummyData"
+import { Link } from "gatsby"
 
 const StyledFooter = styled.footer`
+  display: flex;
   width: 100%;
-  height: 500px;
   background-color: ${({ theme }) => theme.font.black};
 `
-
-const StyledContentWrapper = styled.div`
+const StyledContainer = styled.div`
   display: flex;
-  justify-content: space-between;
-  align-items: center;
-  max-width: 1280px;
+  flex-direction: column;
+  width: 1280px;
   margin: 0 auto;
-  color: ${({ theme }) => theme.font.white};
+  color: white;
+`
+const StyledCategoryWrapper = styled.div`
+  display: flex;
+
+  ${down("sm")} {
+    display: none;
+  }
 `
 
-const StyledInfoWrapper = styled.div`
+const StyledCategory = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1 0 auto;
+  margin: 80px 20px;
+`
+const StyledTitle = styled.h3`
+  font-size: 14px;
+  text-transform: uppercase;
+  margin-bottom: 40px;
+`
+
+const StyledList = styled.ul`
   display: flex;
   flex-direction: column;
 `
 
-const StyledLink = styled.a`
-  &:nth-child(2) {
-    margin: 0 40px;
-  }
+const StyledListItem = styled.li`
+  font-size: 13px;
+  margin-bottom: 15px;
+`
 
-  img {
-    width: 30px;
-    height: 30px;
+const StyledListItemLink = styled(Link)`
+  color: ${({ theme }) => theme.font.tertiary};
+  &:hover {
+    color: ${({ theme }) => theme.font.white};
   }
 `
 
-const StyledTypography = styled.span`
-  margin-bottom: 10px;
+const StyledAboutMe = styled.div`
+  color: ${({ theme }) => theme.font.tertiary};
+  font-size: 13px;
+  margin-bottom: 15px;
+`
+
+const StyledRightReserved = styled.div`
+  padding: 15px;
+  font-size: 13px;
+  color: ${({ theme }) => theme.font.tertiary};
+  text-align: center;
 `
 
 const Footer = () => {
   return (
     <StyledFooter>
-      <StyledContentWrapper>
-        <div>
-          <StyledLink target="_blank" href="https://github.com/mateuszmatysiak">
-            <img src={GithubIcon} alt="Github icon" />
-          </StyledLink>
-          <StyledLink
-            target="_blank"
-            href="https://www.linkedin.com/in/mateusz-matysiak/"
-          >
-            <img src={LinkedinIcon} alt="Linkedin icon" />
-          </StyledLink>
-          <StyledLink target="_blank" href="https://twitter.com/matys0">
-            <img src={TwitterIcon} alt="Twitter icon" />
-          </StyledLink>
-        </div>
-        <StyledInfoWrapper>
-          <StyledTypography>Mateusz Matysiak</StyledTypography>
-          <StyledTypography>testowyxyz@gmail.com</StyledTypography>
-          <StyledTypography>123 456 789</StyledTypography>
-        </StyledInfoWrapper>
-      </StyledContentWrapper>
+      <StyledContainer>
+        <StyledCategoryWrapper>
+          <StyledCategory>
+            <StyledTitle>Kategorie</StyledTitle>
+            <StyledList>
+              {mainNavigation.map(({ name, to }) => (
+                <StyledListItem key={name}>
+                  <StyledListItemLink to={to}>{name}</StyledListItemLink>
+                </StyledListItem>
+              ))}
+            </StyledList>
+          </StyledCategory>
+          <StyledCategory>
+            <StyledTitle>Frontsource</StyledTitle>
+            <StyledList>
+              {frontsourceTest.map(({ name, to }) => (
+                <StyledListItem key={name}>
+                  <StyledListItemLink to={to}>{name}</StyledListItemLink>
+                </StyledListItem>
+              ))}
+            </StyledList>
+          </StyledCategory>
+          <StyledCategory>
+            <StyledTitle>O mnie</StyledTitle>
+            <StyledAboutMe>Mateusz Matysiak</StyledAboutMe>
+            <StyledAboutMe>testmail@gmail.com</StyledAboutMe>
+            <StyledAboutMe>123 456 789</StyledAboutMe>
+          </StyledCategory>
+        </StyledCategoryWrapper>
+        <StyledRightReserved>
+          © All rights reserved 2020. Mateusz Matysiak
+        </StyledRightReserved>
+      </StyledContainer>
     </StyledFooter>
   )
 }
---
+++ src/utils/dummyData.js
@@ -0,0 +1,7 @@
+export const frontsourceTest = [
+  { name: "Test123", to: "/blog" },
+  { name: "Test troche dłuższy", to: "/frontsource" },
+  { name: "Test test ", to: "/about" },
+  { name: "Test 123 321 Test", to: "/contact" },
+  { name: "555 testów, które są długie", to: "/contact" },
+]
