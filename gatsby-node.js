63ed572a871a716eb70ca9f5868e9339c204d7e7
Mateusz <mateuszmatysiak96@gmail.com>
2020-06-19 08:14:29 +0200

    Fix gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,8 +4,7 @@ const slugify = require("slugify")
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
   const blogPostTemplate = path.resolve(`src/layouts/post.js`)
-  const categoryTemplate = path.resolve(`src/layouts/category.js`)
-  const articles = graphql(
+  const { data } = await graphql(
     `
       query queryCMSPage {
         allDatoCmsArticle {
@@ -16,13 +15,9 @@ exports.createPages = async ({ graphql, actions }) => {
         }
       }
     `
-  ).then(result => {
-    if (result.errors) {
-      Promise.reject(result.errors)
-    }
-  })
+  )
 
-  articles.allDatoCmsArticle.nodes.forEach(post => {
+  data.allDatoCmsArticle.nodes.forEach(post => {
     const slugifiedTitle = slugify(post.title, { lower: true })
     createPage({
       path: `blog/${slugifiedTitle}`,
@@ -32,34 +27,67 @@ exports.createPages = async ({ graphql, actions }) => {
       },
     })
   })
-
-  // const tags = graphql(`
-  //   {
-  //     allDatoCmsTag {
-  //       edges {
-  //         node {
-  //           tag
-  //           tagDescription
-  //         }
-  //       }
-  //     }
-  //   }
-  // `).then(result => {
-  //   if (result.errors) {
-  //     Promise.reject(result.errors)
-  //   }
-
-  //   tags.allDatoCmsTag.edges.forEach(({ tag, tagDescription }) => {
-  //     createPage({
-  //       path: `blog/category/${tag}`,
-  //       component: categoryTemplate,
-  //       context: {
-  //         id: tag,
-  //         tagDescription,
-  //       },
-  //     })
-  //   })
-  // })
-
-  // return Promise.all([articles, tags])
 }
+
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { createPage } = actions
+//   const blogPostTemplate = path.resolve(`src/layouts/post.js`)
+//   const categoryTemplate = path.resolve(`src/layouts/category.js`)
+//   const articles = graphql(
+//     `
+//       query queryCMSPage {
+//         allDatoCmsArticle {
+//           nodes {
+//             title
+//             id
+//           }
+//         }
+//       }
+//     `
+//   ).then(result => {
+//     if (result.errors) {
+//       Promise.reject(result.errors)
+//     }
+//   })
+
+//   articles.allDatoCmsArticle.nodes.forEach(post => {
+//     const slugifiedTitle = slugify(post.title, { lower: true })
+//     createPage({
+//       path: `blog/${slugifiedTitle}`,
+//       component: blogPostTemplate,
+//       context: {
+//         id: post.id,
+//       },
+//     })
+//   })
+
+// const tags = graphql(`
+//   {
+//     allDatoCmsTag {
+//       edges {
+//         node {
+//           tag
+//           tagDescription
+//         }
+//       }
+//     }
+//   }
+// `).then(result => {
+//   if (result.errors) {
+//     Promise.reject(result.errors)
+//   }
+
+//   tags.allDatoCmsTag.edges.forEach(({ tag, tagDescription }) => {
+//     createPage({
+//       path: `blog/category/${tag}`,
+//       component: categoryTemplate,
+//       context: {
+//         id: tag,
+//         tagDescription,
+//       },
+//     })
+//   })
+// })
+
+// return Promise.all([articles, tags])
+// }
